0315252ddb79d1c59412b9911e6ca2fb32c921b0
TLZZ okey <tolazzi@et.esiea.fr>
2022-01-20 13:44:01 -0500

    Singleton class created

---
+++ src/main/java/com/java/main/Main.java
@@ -0,0 +1,11 @@
+package main.java.com.java.main;
+
+import main.java.com.java.singleton.Singleton;
+
+public class Main {
+    public static void main(String[] args){
+        Singleton robot = new Singleton();
+        System.out.println("New Singleton created");
+        System.out.println("Hello World!!!");
+    }
+}
---
+++ src/main/java/com/java/singleton/Singleton.java
@@ -0,0 +1,37 @@
+package main.java.com.java.singleton;
+
+import java.awt.*;
+
+public class Singleton {
+    private static Singleton robot = null;
+    private boolean penState;
+    private Point coordinates;
+
+    public Singleton(){
+        this.penState = false;
+        coordinates = new Point(0,0);
+    }
+
+    public static Singleton getInstance(){
+        if (robot == null)
+            robot = new Singleton();
+
+        return robot;
+    }
+
+    public boolean getPenState(){
+        return this.penState;
+    }
+
+    public void setPenState(){
+        this.penState ^= true;
+    }
+
+    public Point getCoordinates(){
+        return this.coordinates;
+    }
+
+    public void setCoordinates(Point p){
+        this.coordinates = p;
+    }
+}
