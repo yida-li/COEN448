74ccd2e01c455068c97cf62a9c1e80ed2761591a
yida-li <gabrielyidali@gmail.com>
2022-01-25 20:49:04 -0500

    testing out gui for the grid to move the robot

--- src/main/java/com/java/main/Main.java
+++ src/main/java/com/java/main/Main.java
@@ -2,10 +2,19 @@ package main.java.com.java.main;
 
 import main.java.com.java.singleton.Singleton;
 
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         Singleton robot = Singleton.getInstance();
         System.out.println("Robot created !");
         System.out.println("Hello World!!!");
+        new Grid();
     }
+
+
 }
+
